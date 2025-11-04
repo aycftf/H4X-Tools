97cb6ff0140c68bf3ef65590bb59dbf7dd34b665
Alexander Carter <aycarter2005@gmail.com>
2025-11-04 13:11:11 -0800

    update setup.sh
    add Python installation check and update script bash syntax

--- setup.sh
+++ setup.sh
@@ -10,6 +10,19 @@ echo "-- Made in Finland, by Vili."
 echo
 echo "You may need to install 'python-devel' packages."
 
+
+##check for python installation
+#TODO; add auto option to symlink or alias python2 or python w/ python3
+check=$(python --version)
+if [ -z "$check" ]; then 
+    echo "installing python3 packages"
+    sudo apt-get update && sudo apt-get upgrade -y --show-progress && sudo apt-get dist-upgrade -y && sudo apt install python3 python3-pip python3-dev python3-venv build-essential || sudo dnf5 update && sudo dnf5 install python3 python3-pip python3-dev python3-venv build-essential
+    #todo: check python path is correct to py3
+    if which python3 && echo $PYTHONPATH >/dev/null 2>&1
+        echo -e "\n python packages up to date"
+    fi
+fi
+
 # Create and activate virtual environment
 echo "Creating virtual environment..."
 python3 -m venv .venv
@@ -27,9 +40,10 @@ fi
 
 # Build H4X-Tools to a single executable
 echo "Building H4X-Tools to a single executable..."
+user=$(whoami)
 if command -v pyinstaller >/dev/null 2>&1; then
     pyinstaller h4xtools.py --add-data "resources/*:resources" --onefile -F --clean
-    chmod +x dist/h4xtools
+    chmod +x dist/h4xtools && chown -R "$user:$user" dist/h4xtools
     if [ -d "$HOME/.local/bin" ]; then
         mv dist/h4xtools "$HOME/.local/bin"
     else
@@ -37,11 +51,10 @@ if command -v pyinstaller >/dev/null 2>&1; then
         mv dist/h4xtools "$HOME/.local/bin"
     fi
     rm h4xtools.spec
-    rm -r build
-    rm -r dist
-    echo "Done! Type h4xtools in your terminal to start!"
-    read -r -p "Do you want to start H4XTools now? (y/N) " answer
-    if [[ $answer == "y" ||  $answer == "Y" || $answer == "yes" || $answer == "Yes" ]]; then
+    rm -r dist && rm -r build
+    echo -e "\n Done! Type h4xtools in your terminal to start!"
+    read -r -p "Do you want to start H4XTools now? (y/N) " -en 3 answer
+    if [[ $answer =~ Y|y|yes|Yes ]]; then
         h4xtools
     fi
 else
